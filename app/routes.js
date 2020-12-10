0783a49705b09e0fdab33cf3c1b4c4dc93d6de40
Bruno Gonçalves <bugotech@gmail.com>
2020-12-10 10:51:38 -0300

    add search resource

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,9 @@
-const InquilinoController = require('./controllers/InquilinoController');
 const { Inquilino } = require('./models');
+//const InquilinoController = require('./controllers/InquilinoController');
 
 module.exports = (app) => {
 
     //InquilinoController.register(app);
 
-    app.resource('/inquilinos', Inquilino, 'Inquilino');
+    app.resource('/inquilinos', Inquilino, 'Inquilino', { search: ['ns','nome']});
 }
